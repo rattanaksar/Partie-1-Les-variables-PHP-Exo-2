6dbebf536bb31fe86d60b26492104a04492289b7
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-06-09 10:14:45 +0200

    Exo 2 PHP

---
+++ Exo 2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo 2 : Les variables PHP</title>
+</head>
+<body>
+    <h1>Exo 2 Partie : Les variables PHP</h1>
+    <?php
+        $firstName = "Sar";
+        $lastName = "Rattanak";
+        $age="38";
+        echo "Mon nom est $firstName et mon prénom est $lastName et j'ai $age ans ";
+    ?>
+</body>
+</html>
